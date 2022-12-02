91e1222e98aed32214df716b89a5e582f595a2bb
Bravin Murega <97433032+Murega14@users.noreply.github.com>
2022-12-02 03:23:20 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return(sub(a, b))
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
+
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    result = add(int(argv[1]), int(argv[3]))
+elif argv[2] == '-':
+    result = sub(int(argv[1]), int(argv[3]))
+elif argv[2] == '*':
+    result = mul(int(argv[1]), int(argv[3]))
+elif argv[2] == '/':
+    result = div(int(argv[1]), int(argv[3]))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
