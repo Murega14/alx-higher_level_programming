2a64909a8d67ed7d679baee896a87baeb2f1ca1b
Bravin Murega <97433032+Murega14@users.noreply.github.com>
2022-12-07 16:47:22 +0300

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
+for a in range(122, 96, -1):
     if(a % 2 != 0):
-        a += 32
-    print("{:s)".format(chr(i)), end="")
+        a -= 32
+    print("{:s}".format(chr(a)), end="")
