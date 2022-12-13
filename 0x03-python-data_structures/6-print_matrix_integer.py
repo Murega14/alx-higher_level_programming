5af90a16ad5c9a831bc9b58f5f9df96bf4a6d4eb
murega14 <tedmurega@gmail.com>
2022-12-13 07:35:48 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        row_len = len(row)
+        for x in range(row_len):
+            if x != row_len = 1:
+                print("{:d}".format(row[x]), end=' ')
+            else:
+                print("{:d}".format(row[x], end='')
+        print()
