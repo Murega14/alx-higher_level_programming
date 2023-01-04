eb81367d7df1b493935dca8e2ee312030f4061cd
murega14 <tedmurega@gmail.com>
2023-01-04 09:56:02 +0000

    task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_square(size):
+    if size is 0:
+        return
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >=0')
+    print('\n'.join('#' * size for x in range(size)))
