d6027a42084d45bf96b4557508d8a986212b7a19
Bravin Murega <tedmurega@gmail.com>
2023-01-10 23:24:39 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,10 +2,13 @@
 """returns number of lines"""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
     """number of lines"""
     lines = 0
     with open(filename, mode='r', encoding='utf-8') as f:
         for line in f:
             lines += 1
+    with open(filename="", mode="w" encoding="utf-8") as f:
+        return f.write(text)
     return lines
+    
