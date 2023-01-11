b897c41653ec4b058f8caa13eb96a353b4ae9fc6
Bravin Murega <tedmurega@gmail.com>
2023-01-11 10:46:22 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""defines a string-to-JSON function."""
+"""Defines a string-to-JSON function."""
 import json
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of a string object"""
+    """Returns the JSON representation of a string object."""
     return json.dumps(my_obj)
 
