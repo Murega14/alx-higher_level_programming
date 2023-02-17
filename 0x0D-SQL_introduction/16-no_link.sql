f5ac8073a97496161deb4a712e5f21bee6ccfa86
murega14 <tedmurega@gmail.com>
2023-02-17 12:28:02 +0300

    sql intro

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table with no name order
 SELECT score, name FROM second_table
-WHERE name <> ''
-GROUP BY score DESC, name DESC;
+WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
+
 
 
