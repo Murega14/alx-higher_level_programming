6c0dd04b4292d851aab39f3da87ed018e29bd031
murega14 <tedmurega@gmail.com>
2023-02-18 12:33:11 +0300

    advanced task 2

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- lists all shows that are not in the comedy genre
+SELECT title FROM tv_shows
+WHERE title NOT IN (SELECT ts.title FROM tv_shows AS ts
+JOIN tv_show_genres AS tv_show_genres
+ON ts.id = tgs.show_id
+JOIN tv_genres AS tgg
+ON tgs.genre_id = tgg.id
+WHERE tgg.name = "Comedy")
+ORDER BY title;
