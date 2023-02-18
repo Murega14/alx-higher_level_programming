9d9b80f8b76c34040f7278d45288cb8031af6fc4
murega14 <tedmurega@gmail.com>
2023-02-18 11:55:38 +0300

    task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- database dump from hbtn_0d_tvshows
+SELECT tgg.name FROM tv_genres AS tgg
+JOIN tv_show_genres AS tgs ON tgs.genre_id = tgg.id
+JOIN tv_shows AS ts ON ts.id = tgs.show_id
+WHERE ts.title = Dexter
+GROUP BY tgg.name ASC;
