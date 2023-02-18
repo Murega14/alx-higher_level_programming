aab1be238a4416751ff8533cf9f1cdd9f59db06a
murega14 <tedmurega@gmail.com>
2023-02-18 11:09:04 +0300

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database
 SELECT c.id, c.name, s.name FROM cities c, states s
-WHERE c.state_id=s.id
-GROUP BY c.id ASC;
+INNER JOIN states ON cities.state_id=s.id
+ORDER BY c.id;
