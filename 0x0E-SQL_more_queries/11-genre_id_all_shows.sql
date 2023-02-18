f4d7a7871255b3f267277880fffce9934cf3f079
murega14 <tedmurega@gmail.com>
2023-02-18 11:19:38 +0300

    task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Import the database dump of hbtn_0d_tvshow
+SELECT ts.title, tg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
+ORDER BY ts.title ASC, tg.genre_id ASC;
