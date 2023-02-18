38bc0c41210f5dc549b658a51474e0f7d2e8ef12
murega14 <tedmurega@gmail.com>
2023-02-18 12:00:11 +0300

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- database dump from hbtn_0d_tvshows
-SELECT tgg.name FROM tv_genres AS tgg
-JOIN tv_show_genres AS tgs ON tgs.genre_id = tgg.id
-JOIN tv_shows AS ts ON ts.id = tgs.show_id
-WHERE ts.title = 'Dexter'
-GROUP BY tgg.name ASC;
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows on tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
